1910d0174ef4d3aaf32a0348bb5d2c5f90456b4e
gopikavd2005-creator <gopikavd2005@gmail.com>
2025-11-13 23:04:17 +0530

    Add files via upload

---
+++ src/ifcondition.java
@@ -0,0 +1,8 @@
+public class ifcondition{
+    public static void main(String[]args){
+        int n=10;
+        if(n>0){
+            System.out.println("Positive number");
+        }
+    }
+}
---
+++ src/ifelseladder.java
@@ -0,0 +1,22 @@
+public class ifelseladder{
+    public static void main(String[]args){
+        int mark=75;
+        if(mark>=90){
+            System.out.println("A Grade");
+        }
+        else if(mark>=80){
+            System.out.println("B Grade");
+        }
+        else if(mark>=60){
+            System.out.println("c Grade");
+        }
+        else if(mark>=40){
+            System.out.println("d Grade");
+        }
+        else
+        {
+            System.out.println("Fail");
+        }
+    }
+
+        }
